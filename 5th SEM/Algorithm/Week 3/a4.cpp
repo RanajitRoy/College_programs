6f87c6abc8075b80f9198a2e6640900e1cc2154f
RanajitRoy <royranajit1@gmail.com>
2018-08-08 21:24:26 +0530

    modified

--- 5th SEM/Algorithm/Week 3/a4.cpp
+++ 5th SEM/Algorithm/Week 3/a4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+
 typedef std::vector<std::vector<int>> data;
 
 data merge(const data &a, const data &b, const data &c, const data &d)
@@ -71,12 +72,17 @@ data operator+(const data& a, const data& b)
 
 data product(data X, data Y)
 {
-	if(X.size()==1)
+	int n = X.size();
+	if(n==2)
 	{
-		data bound(1,std::vector<int>(1,X[0][0]*Y[0][0]));
+		data bound(2,std::vector<int>(2));
+		bound[0][0] = (X[0][0]*Y[0][0])+(X[0][1]*Y[1][0]);
+		bound[0][1] = (X[0][0]*Y[0][1])+(X[0][1]*Y[1][1]);
+		bound[1][0] = (X[1][0]*Y[0][0])+(X[1][1]*Y[1][0]);
+		bound[1][1] = (X[1][0]*Y[0][1])+(X[1][1]*Y[1][1]);
 		return bound;
 	}
-	if(X.size()%2==1)
+	if(n%2==1)
 	{
 		for(int i=0;i<X.size();i++)
 		{
@@ -92,28 +98,37 @@ data product(data X, data Y)
 	std::tie(A, B, C, D) = slice(X);
 	std::tie(E, F, G, H) = slice(Y);
 	data P1 = product(A, F-H);
-	std::cout<<"Reached\n";
-	data P2 = product(H, A+B);
-	std::cout<<"Reached\n";
-	data P3 = product(E, C+D);
+	data P2 = product(A+B, H);
+	data P3 = product(C+D, E);
 	data P4 = product(D, G-E);
 	data P5 = product(A+D, E+H);
 	data P6 = product(B-D, G+H);
 	data P7 = product(A-C, E+F);
-	return merge((P6+P5)+(P4-P2), P1+P2, P3+P4, (P1+P5)-(P3+P7));
+	auto temp = merge((P6+P5)+(P4-P2), P1+P2, P3+P4, (P1+P5)-(P3+P7));
+	if(n%2==1)
+	{
+		temp.pop_back();
+		for(auto& i: temp)
+			i.pop_back();
+	}
+	return temp;
 }
 
 int main()
 {
 	int n;
+	std::cout<<"Enter n: ";
 	std::cin>>n;
 	data mtx1(n, std::vector<int>(n)), mtx2(n, std::vector<int>(n));
+	std::cout<<"Enter the 1st matrix:\n";
 	for(auto& i : mtx1)
 		for(auto& j : i)
 			std::cin>>j;
+	std::cout<<"Enter the 2st matrix:\n";
 	for(auto& i : mtx2)
 		for(auto& j : i)
 			std::cin>>j;
+	std::cout<<"The resultant matrix:\n";
 	auto res = product(mtx1,mtx2);
 	for(auto& i : res)
 	{
@@ -121,5 +136,6 @@ int main()
 			std::cout<<j<<", ";
 		std::cout<<"\n";
 	}
+	std::cout<<std::endl;
 	return 0;
 }
