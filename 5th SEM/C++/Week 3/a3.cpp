38f3a436a569f9df024e501951813a27a56e398e
RanajitRoy <royranajit1@gmail.com>
2018-08-12 03:11:42 +0530

    inclusion

--- 5th SEM/C++/Week 3/a3.cpp
+++ 5th SEM/C++/Week 3/a3.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 
 template<typename T>
-class Matrix
+struct Matrix
 {
-	std::vector<std::vector<T>> &mat;
-	mat(int n, int m)
+	std::vector<std::vector<T>> mat;
+	Matrix(int n, int m)
 	{
-		mat = std::vector<std::vector<T>>(n, std::vector<T>(m));
+		mat = std::vector<std::vector<T>>(n, std::vector<T>(m, 0));
 	}
-public:
 	void initialize()
 	{
 		for(auto &i: mat)
@@ -22,7 +21,7 @@ public:
 		{
 			for(auto &j: i)
 				std::cout<<"\t"<<j;
-			std::endl;
+			std::cout<<std::endl;
 		}
 	}
 	Matrix<T> operator+(Matrix<T> &a)
@@ -31,7 +30,7 @@ public:
 		
 		for(int i=0;i<mat.size();i++)
 			for(int j=0;j<mat[i].size();j++)
-				res[i][j]=mat[i][j]+a[i][j];
+				res.mat[i][j]=mat[i][j]+a.mat[i][j];
 		return res;
 	}
 	Matrix<T> operator-(Matrix<T> &a)
@@ -40,17 +39,17 @@ public:
 		
 		for(int i=0;i<mat.size();i++)
 			for(int j=0;j<mat[i].size();j++)
-				res[i][j]=mat[i][j]-a[i][j];
+				res.mat[i][j]=mat[i][j]-a.mat[i][j];
 		return res;
 	}
 	Matrix<T> operator*(Matrix<T> &a)
 	{
-		Matrix<T> res(mat.size(), a[0].size());
+		Matrix<T> res(mat.size(), a.mat[0].size());
 		
 		for(int i=0;i<mat.size();i++)
-			for(int j=0;j<a[0].size();j++)
+			for(int j=0;j<a.mat[0].size();j++)
 				for(int k=0;k<mat[0].size();k++)
-					res[i][j]=mat[i][k]+a[k][j];
+					res.mat[i][j]+=mat[i][k]*a.mat[k][j];
 		return res;
 	}
 };
@@ -62,15 +61,26 @@ int main()
 	std::cin>>n>>m;
 	Matrix<int> A(n, m), B(n, m);
 	Matrix<double> C(n, m), D(n, m);
+	std::cout<<"Enter the matrix A: \n";
 	A.initialize();
+	std::cout<<"Enter the matrix B: \n";
 	B.initialize();
+	std::cout<<"Enter the matrix C: \n";
 	C.initialize();
+	std::cout<<"Enter the matrix D: \n";
 	D.initialize();
-	(A+B).display()
+	std::cout<<"\nThe matrix A+B:\n";
+	(A+B).display();
+	std::cout<<"The matrix A-B:\n";
 	(A-B).display();
+	std::cout<<"The matrix A*B:\n";
 	(A*B).display();
-	(C+D).display()
+	std::cout<<"The matrix C+D:\n";
+	(C+D).display();
+	std::cout<<"The matrix C-D:\n";
 	(C-D).display();
+	std::cout<<"The matrix C*D:\n";
 	(D*C).display();
+	std::cout<<std::endl;
 	return 0;
 }
