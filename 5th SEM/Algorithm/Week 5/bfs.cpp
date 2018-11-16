8cf96423d19a6594ad2b856e49f920d7c5ab5325
RanajitRoy <royranajit1@gmail.com>
2018-11-16 21:46:25 +0530

    modified

--- 5th SEM/Algorithm/Week 5/bfs.cpp
+++ 5th SEM/Algorithm/Week 5/bfs.cpp
@@ -6,38 +6,38 @@
 int main()
 {
 	std::cout<<"Enter the no. of vertices & edges: ";
-    int v, e; std::cin>>v>>e;
-    std::vector<std::list<int>> adjList(v);
+	int v, e; std::cin>>v>>e;
+	std::vector<std::list<int>> adjList(v);
 	std::cout<<"Enter the edges with adjacent vertices:\n";
 	for(int i = 0; i < e; i++)
 	{
 		int x, y; std::cin>>x>>y;
 		adjList[x].push_back(y);
 	}
-    std::vector<bool> visited(v,false);
-    std::vector<int> depth(v,0);
+	std::vector<bool> visited(v,false);
+	std::vector<int> depth(v,0);
 	std::cout<<"Enter the source: ";
-    int start; std::cin>>start;
-    std::queue<int> q;
-    q.push(start);
-    while(!q.empty())
-    {
-        int x = q.front();
-        q.pop();
-        visited[x] = true;
-        for(auto &i: adjList[x])
-        {
-            if(!visited[i])
-            {
-                q.push(i);
-                visited[i] = true;
-                depth[i] = depth[x] + 1;
-            }
-        }
-    }
-    std::cout<<"Depth for each vertex:\n";
-    for(auto i=0;i<v;i++)
-    {
-        std::cout<<i<<" -> "<<depth[i]<<"\n";
-    }
+	int start; std::cin>>start;
+	std::queue<int> q;
+	q.push(start);
+	while(!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		visited[x] = true;
+		for(auto &i: adjList[x])
+		{
+			if(!visited[i])
+			{
+				q.push(i);
+				visited[i] = true;
+				depth[i] = depth[x] + 1;
+			}
+		}
+	}
+	std::cout<<"Depth for each vertex:\n";
+	for(auto i=0;i<v;i++)
+	{
+		std::cout<<i<<" -> "<<depth[i]<<"\n";
+	}
 }
